dfa40950bdd801d387f8cc5280b17cb5f34158cc
SashaKor <akoroza@stuy.edu>
2019-05-13 19:59:44 -0400

    signing up and authorizing

--- utils/api.py
+++ utils/api.py
@@ -1,13 +1,65 @@
 import json
 import urllib.request as request
 import datetime
+import fitbit
+import gather_keys_oauth2 as Oauth2
+#import pandas as pd
 
+CLIENT_ID = '22DPT8'
+CLIENT_SECRET = '75bdab2bae6505734820d543dee4fb21'
+'''
+Application registered on https://dev.fitbit.com/apps/details/22DPT8 with akoroza@stuy.edu
+Following https://towardsdatascience.com/collect-your-own-fitbit-data-with-python-ff145fa10873 tutorial
+
+OAuth 2.0 Client ID ***
+22DPT8
+
+Client Secret ***
+75bdab2bae6505734820d543dee4fb21
+
+Callback URL
+http://127.0.0.1:8080/
+OAuth 2.0: Authorization URI
+https://www.fitbit.com/oauth2/authorize
+OAuth 2.0: Access/Refresh Token Request URI
+https://api.fitbit.com/oauth2/token
+'''
+'''
+Using the ID and Secret, we can obtain the access and refresh tokens that authorize us to get our data.
+'''
+server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+server.browser_authorize()
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+'''
+Step 2 calls for an unofficial Fitbit API
+python-fitbit (a complete python implementation of the Fitbit API.)
+docs: https://python-fitbit.readthedocs.io/en/latest/
+gh: https://github.com/orcasgit/python-fitbit
+
+Extra requirements:
+python-dateutil>=1.5   <-- The dateutil module provides powerful extensions to the datetime module available in the Python standard library.
+requests-oauthlib>=0.7 <-- for accessing protected resources from Fitbit
+
+pip3 install fitbit
+cherrypy
+'''
+'''
+Formatting dates for api calls
+'''
+yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))
+yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+today = str(datetime.datetime.now().strftime("%Y%m%d"))
+
+
+'''
 def access_info(URL_STUB, API_KEY = None, **kwargs):
-    '''
-    Helper to access the info for a URL. Returns the JSON.
-    Params: URL_STUB, API_KEY = None, **kwargs for applying headers to requests
-    NOTE: API_KEY should only be used if the key can be put in the URL. Otherwise, use **kwargs.
-    '''
+'''
+    #Helper to access the info for a URL. Returns the JSON.
+    #Params: URL_STUB, API_KEY = None, **kwargs for applying headers to requests
+    # API_KEY should only be used if the key can be put in the URL. Otherwise, use **kwargs.
+'''
     # if there's an API key that is not a header
     if API_KEY:
         URL = URL_STUB + API_KEY
@@ -27,11 +79,11 @@ def access_info(URL_STUB, API_KEY = None, **kwargs):
     return info
 
 def get_bored_activity():
-    '''
-    Fetches an activity and returns that activity.
-    Returns a dict with:
-    - activity: the activity
-    '''
+'''
+    #Fetches an activity and returns that activity.
+    #Returns a dict with:
+    #- activity: the activity
+'''
     data = access_info('https://www.boredapi.com/api/activity')
     result = {}
     result['activity'] = data['activity']
@@ -40,12 +92,12 @@ def get_bored_activity():
     return result
 
 def get_quote():
-    '''
-    Returns a random quote and the author who said/wrote it.
-    Returns a dict with:
-    - quote: the quote
-    - author: the author
-    '''
+'''
+    #Returns a random quote and the author who said/wrote it.
+    #Returns a dict with:
+    #- quote: the quote
+    #- author: the author
+'''
     data = access_info('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=')
     result = {}
 
@@ -63,12 +115,12 @@ def get_quote():
     return result
 
 def get_word(query):
-    '''
-    Finds 100 possible words using the query parameter.
-    Returns a dict with:
-    - query: the query
-    - words: a list of all possible words
-    '''
+'''
+    #Finds 100 possible words using the query parameter.
+    #Returns a dict with:
+    #- query: the query
+    #- words: a list of all possible words
+'''
     query = query.strip()
 
     #modify spacing
@@ -87,12 +139,12 @@ def get_word(query):
     return result
 
 def get_definition(query):
-    '''
-    Finds all possible defintions using the query parameter.
-    Returns a dict with:
-    - word: the query
-    - definitions: a list of all possible definitions
-    '''
+'''
+    #Finds all possible defintions using the query parameter.
+    #Returns a dict with:
+    #- word: the query
+    #- definitions: a list of all possible definitions
+'''
     query = query.strip()
     file = open("api/oxford.txt", 'r').read()
     apikey = file.strip()
@@ -135,7 +187,7 @@ def get_definition(query):
 
     #print('result:',result)
     return result
-
+'''
 if __name__ == '__main__':
     data = None
     print(get_word('djkfhnbglrjboierjriorjtotijgoirj'))
