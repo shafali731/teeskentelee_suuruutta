d2ebda23e59c84956ca1773c5ef5a013e8b569bd
SashaKor <akoroza@stuy.edu>
2019-05-13 20:27:46 -0400

    testing

--- utils/api.py
+++ utils/api.py
@@ -10,6 +10,7 @@ CLIENT_SECRET = '75bdab2bae6505734820d543dee4fb21'
 '''
 Application registered on https://dev.fitbit.com/apps/details/22DPT8 with akoroza@stuy.edu
 Following https://towardsdatascience.com/collect-your-own-fitbit-data-with-python-ff145fa10873 tutorial
+https://github.com/stephenjhsu/fitbit/blob/master/fitbit.py
 
 OAuth 2.0 Client ID ***
 22DPT8
@@ -23,16 +24,7 @@ OAuth 2.0: Authorization URI
 https://www.fitbit.com/oauth2/authorize
 OAuth 2.0: Access/Refresh Token Request URI
 https://api.fitbit.com/oauth2/token
-'''
-'''
-Using the ID and Secret, we can obtain the access and refresh tokens that authorize us to get our data.
-'''
-server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
-server.browser_authorize()
-ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
-REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
-auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
-'''
+
 Step 2 calls for an unofficial Fitbit API
 python-fitbit (a complete python implementation of the Fitbit API.)
 docs: https://python-fitbit.readthedocs.io/en/latest/
@@ -44,13 +36,27 @@ requests-oauthlib>=0.7 <-- for accessing protected resources from Fitbit
 
 pip3 install fitbit
 cherrypy
+numpy
 '''
+
+'''
+Using the ID and Secret, we can obtain the access and refresh tokens that authorize us to get our data.
+'''
+server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+server.browser_authorize()
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
 '''
 Formatting dates for api calls
 '''
 yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))
 yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
 today = str(datetime.datetime.now().strftime("%Y%m%d"))
+
+
+#fit_statsHR = auth2_client.intraday_time_series('activities/heart', base_date=yesterday2, detail_level='1sec')
 
 
 '''
